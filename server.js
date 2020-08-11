db6e4da0fd713ccec28f5c30f93ed2efa6f8ae02
Joshua Blank <jmnblnk@yahoo.com>
2020-08-10 19:05:18 -0700

    work on config

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("hello world!");
+// Dependencies add sequel and sequelize, handlebars
+// =============================================================
+const express = require("express");
+// const mysql = require("mysql");
+// const exphbs = require("express-handlebars");
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("#"));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
