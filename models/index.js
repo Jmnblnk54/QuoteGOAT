e61e34c152711d100fba5a16a0d058e4d81b73db
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-10 19:56:30 -0700

    fix quotes

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config');
+const config = require("../config/config");
 const db = {};
 
 console.log(config);
@@ -14,7 +13,7 @@ const sequelize = new Sequelize(config.development);
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
