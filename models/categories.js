f9ab5bc420c212ac3514f73b57cca0b560608aa6
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-13 00:18:46 -0700

    add categories seed data

--- models/categories.js
+++ models/categories.js
@@ -1,13 +1,13 @@
 module.exports = function (sequelize, DataTypes) {
   var Category = sequelize.define("Category", {
-    categoryId: {type: DataTypes.INTEGER, allowNull: false, primaryKey:true},
-    categoryrName: {type: DataTypes.STRING, allowNull: false}, });
+    categoryId: {type: DataTypes.INTEGER, allowNull: false, primaryKey:true, autoIncrement:true},
+    categoryName: {type: DataTypes.STRING, allowNull: false}, });
 
-  Category.associate = function (models) {
-    Category.belongsTo(models.Vote, {
-      foreignKey: {
-        allowNull: false}
-    });
-  };
+  // Category.associate = function (models) {
+  //   Category.belongsTo(models.Vote, {
+  //     foreignKey: {
+  //       allowNull: false}
+  //   });
+  // };
   return Category;
 };
