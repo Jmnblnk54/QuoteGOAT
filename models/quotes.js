2ba5333cb7d4d7548b54a756d0c793c0ec1a8b2b
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-12 23:57:00 -0700

    add quotes seed data

--- models/quotes.js
+++ models/quotes.js
@@ -1,18 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
   var Quote = sequelize.define("Quote", {
-    quoteId: {type: DataTypes.INTEGER, allowNull: false, primaryKey:true},
+    quoteId: {type: DataTypes.INTEGER, allowNull: false, primaryKey:true, autoIncrement: true},
     categoryId: {type: DataTypes.INTEGER, allowNull: false},
     userId: {type: DataTypes.INTEGER, allowNull: false},
-    quote: {type: DataTypes.STRING, allowNull: false},
-    dateAdded: {type: DataTypes.DATE, allowNull: false} });
+    quote: {type: DataTypes.STRING, allowNull: false} });
 
-  Quote.associate = function (models) {
-    Quote.belongsTo(models.Vote, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
-  console.log("Quote is: ", Quote);
+  // Quote.associate = function (models) {
+  //   Quote.belongsTo(models.Vote, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+  // console.log("Quote is: ", Quote);
   return Quote;
 };
---
+++ seeders/20200813064041-create-quotes.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Quotes",
+      [{
+        categoryId:1,
+        userId: 6,
+        quote:"Do not offend the office manager. He is subject to take back the keys to the shithouse. Always keeps it locked so no sinister stranger can sneak a shit and give all of the kids in the office some horrible condition.",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      },{
+        categoryId:2,
+        userId: 5,
+        quote:"A man has got to know his limitations.",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      },{
+        categoryId:3,
+        userId: 4,
+        quote:"Let them eat snek.",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      },{
+        categoryId:4,
+        userId: 3,
+        quote:"Give me liberty or give me unlimited trips to the salad bar.",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      },{
+        categoryId:5,
+        userId: 2,
+        quote:"I don't care...do you?",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      },{
+        categoryId:6,
+        userId: 1,
+        quote:"What happens to a dream deferred? Does it dry up Like a raisin in the sun? Or fester like a sore...and then run? Does it stink like rotten meat? Or crust and sugar over...like a syrupy sweet? Maybe it just sags like a heavy load. Or does it explode?",
+        createdAt: new Date(),
+        UpdatedAt: new Date()
+      }],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Quotes", null, {});
+  }
+};
