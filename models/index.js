0d576ecda6934be8424b1ac6e35224c6d3d40148
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-15 10:58:08 -0700

    set up for heroku

--- models/index.js
+++ models/index.js
@@ -4,11 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const config = require("../config/config");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 const db = {};
 
 
-const sequelize = new Sequelize(config.development);
+const sequelize = new Sequelize(config);
 
 fs
   .readdirSync(__dirname)
